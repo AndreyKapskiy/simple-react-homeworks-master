ff2b84e32a5e195877e8cf31422f731ed90fbdef
Andrey Kapskiy <akapsky@yandex.by>
2022-07-27 20:02:07 +0300

    Updates

---
+++ precache-manifest.2bd6fdfb401120fe79524f0d4561db56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e65deb74a4735d33d75807de63ad9a",
+    "url": "/simple-react-homeworks-master/index.html"
+  },
+  {
+    "revision": "eb3c928ade7411951db4",
+    "url": "/simple-react-homeworks-master/static/css/main.0e383ee3.chunk.css"
+  },
+  {
+    "revision": "7817725018b5f1bcc09b",
+    "url": "/simple-react-homeworks-master/static/js/2.4e1771f9.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/simple-react-homeworks-master/static/js/2.4e1771f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb3c928ade7411951db4",
+    "url": "/simple-react-homeworks-master/static/js/main.8c133305.chunk.js"
+  },
+  {
+    "revision": "f98134447cfdc41c1584",
+    "url": "/simple-react-homeworks-master/static/js/runtime-main.bb5710fa.js"
+  }
+]);
